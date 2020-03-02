df787c96e6dcb645df58be54b690c0080785be31
Ryan <rdonnelly98@gmail.com>
2020-03-02 12:47:14 -0800

    morphing.java

---
+++ src/Morphing.java
@@ -0,0 +1,68 @@
+/*File Exercise1.java
+
+ IAT455 - Workshop week 7
+
+ **********************************************************/
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
+import java.awt.image.WritableRaster;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+class Morphing extends Frame { 
+
+	public Morphing() {
+		// constructor
+		// Get an image from the specified file in the current directory on the
+		// local hard disk.
+		this.setTitle("Morphing");
+		this.setVisible(true);
+
+		//Anonymous inner-class listener to terminate program
+		this.addWindowListener(
+				new WindowAdapter(){//anonymous class definition
+					public void windowClosing(WindowEvent e){
+						System.exit(0);//terminate the program
+					}//end windowClosing()
+				}//end WindowAdapter
+				);//end addWindowListener
+	}// end constructor
+	
+	protected int getRed(int pixel) 
+	{
+		return (pixel >>> 16) & 0xFF;
+	}
+
+	protected int getGreen(int pixel) 
+	{
+		return (pixel >>> 8) & 0xFF;
+	}
+
+	protected int getBlue(int pixel) 
+	{
+		return pixel & 0xFF;
+	}
+
+	public void paint(Graphics g) 
+	{
+		
+	}
+// =======================================================//
+
+	public static void main(String[] args) 
+	{
+	
+		Morphing img = new Morphing();// instantiate this object
+		img.repaint();// render the image
+	
+	}// end main
+}
+// =======================================================//
