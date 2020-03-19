81f731954605f47a614c3a3e0c3d7ed2315f5a1b
ryanfd <rdonnelly98@gmail.com>
2020-03-18 18:39:25 -0700

    beginning triangulator class

--- src/DelaunayTriangulator.java
+++ src/DelaunayTriangulator.java
@@ -1,3 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 // coordinates on plane
 class vertex {
 	public int x, y;
@@ -38,4 +45,64 @@ class triangle {
 	public triangle child2;
 	public triangle child3;
 }
+
+class tree {
+	static PrintWriter screen = new PrintWriter(System.out, true);
+	static BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+	
+	static FileWriter file2;
+	static PrintWriter outputFile;
+	
+	static int triangleNumber = 0; // num of triangles
+	static int triangleNumber1 = 0;
+	static int vertexNumber; // num of points
+	static vertex[] node; // array of points
+	static triangle root; 
+	static edge e;
+	
+	// method provide to privide output file storing edges of triangulation
+	static void init1() throws IOException 
+	{
+		file2 = new FileWriter("results1.txt");
+		outputFile = new PrintWriter(file2);
+	}
+	
+	// read points and store in array of vertices
+	static void init() throws IOException 
+	{
+		FileReader file = new FileReader("t.txt");
+		BufferedReader inputFile = new BufferedReader(file);
+		
+		// read number of points
+		vertexNumber = new Integer(inputFile.readLine()).intValue();
+		node = new vertex[vertexNumber];
+		for (int i=0; i<vertexNumber; i++) {
+			node[i] = new vertex();
+			node[i].x = new Integer(inputFile.readLine()).intValue();
+			node[i].y = new Integer(inputFile.readLine()).intValue();
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
