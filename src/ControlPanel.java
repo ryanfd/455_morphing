062d8c0e7ff89a5788b27952d7745341f76ed9b0
ryanfd <rdonnelly98@gmail.com>
2020-04-02 17:58:05 -0700

    gui is coming along quite well

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -1,13 +1,13 @@
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.FlowLayout;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -18,24 +18,26 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.TitledBorder;
 
+//control panel shows internal data of selected animal, as well as lets user alter program and objects
 public class ControlPanel extends JPanel {
+	
 	private Container bottom, top;
 	private JButton img1, img2, img3;
 	private JButton morphButton;
 	private JLabel label;
 	private JTextField field;
-
 	private Container center;
+	private String currentImg;
 
 	public ControlPanel() {
 		super();
 		setBorder(new TitledBorder("Control Panel"));
-		setComponentsAttributes();
+		setComponentsAtrributes();
 		setLayout(new BorderLayout());
 		add(bottom, BorderLayout.SOUTH);
 		add(top, BorderLayout.NORTH);
-
-
+	
+	
 		label = new JLabel("Selected Image");
 		field = new JTextField(5);
 		field.setFocusable(false);
@@ -44,11 +46,11 @@ public class ControlPanel extends JPanel {
 		center.setLayout(new FlowLayout(FlowLayout.LEFT, 12, 10));
 		center.add(label);
 		center.add(field);
-
+	
 		add(center, BorderLayout.CENTER);
 	}
 
-	private void setComponentsAttributes() {
+	private void setComponentsAtrributes() {
 		BufferedImage buff1 = null; 
 		BufferedImage buff2 = null; 
 		BufferedImage buff3 = null;
@@ -65,76 +67,59 @@ public class ControlPanel extends JPanel {
 		buff1 = getScaledImage(buff1, buff1.getWidth()/10, buff1.getHeight()/10);
 		buff2 = getScaledImage(buff2, buff2.getWidth()/10, buff2.getHeight()/10);
 		buff3 = getScaledImage(buff3, buff3.getWidth()/10, buff3.getHeight()/10);
-
-		// TODO Auto-generated method stub
+		
 		bottom = new Container();
 		bottom.setLayout(new FlowLayout(FlowLayout.LEFT, 12, 5));
 
+		// select img 1
 		ImageIcon icon1 = new ImageIcon(buff1);
 		img1 = new JButton(icon1);
 		img1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-//				ImgPanel.selectedImg = "Image 1";
-				System.out.println("click");
-//				System.out.println(ImgPanel.selectedImg);
+				ImgPanel.img1Bool = true;
 				ImgPanel.img2Bool = false;
 				ImgPanel.img3Bool = false;
-				ImgPanel.img1Bool = true;
-//				field.setText(String.format("Image 1"));
-				System.out.println("Bool1: " + ImgPanel.img1Bool);
-				System.out.println("Bool2: " + ImgPanel.img2Bool);
-				System.out.println("Bool3: " + ImgPanel.img3Bool);
+				currentImg = "Image 1";
 			}
 		});
 
+		// select img 2
 		ImageIcon icon2 = new ImageIcon(buff2);
 		img2 = new JButton(icon2);
 		img2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-//				ImgPanel.selectedImg = "Image 2";
-//				System.out.println(ImgPanel.selectedImg);
+				ImgPanel.img2Bool = true;
 				ImgPanel.img1Bool = false;
 				ImgPanel.img3Bool = false;
-				ImgPanel.img2Bool = true;
-//				field.setText(String.format("Image 2"));
-				System.out.println("Bool1: " + ImgPanel.img1Bool);
-				System.out.println("Bool2: " + ImgPanel.img2Bool);
-				System.out.println("Bool3: " + ImgPanel.img3Bool);
+				currentImg = "Image 2";
 			}
 		});
 
+		// select img 3
 		ImageIcon icon3 = new ImageIcon(buff3);
 		img3 = new JButton(icon3);
 		img3.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-//				ImgPanel.selectedImg = "Image 3";
-//				System.out.println(ImgPanel.selectedImg);
-				ImgPanel.img1Bool = false;
-				ImgPanel.img2Bool = false;
 				ImgPanel.img3Bool = true;
-//				field.setText(String.format("Image 3"));
-				System.out.println("Bool1: " + ImgPanel.img1Bool);
-				System.out.println("Bool2: " + ImgPanel.img2Bool);
-				System.out.println("Bool3: " + ImgPanel.img3Bool);
+				ImgPanel.img2Bool = false;
+				ImgPanel.img1Bool = false;
+				currentImg = "Image 3";
 			}
 		});
 
+		// button to intiate morphing
 		morphButton = new JButton("Morph");
 		morphButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				if (!ImgPanel.doMorph) ImgPanel.doMorph = true;
-				System.out.println(ImgPanel.doMorph);
+				ImgPanel.doMorph = true;
 			}
 		});
 
+		//add all buttons
 		top = new Container();
 		top.setLayout(new FlowLayout(FlowLayout.CENTER, 100, 10));
 		top.add(img1);
@@ -144,13 +129,16 @@ public class ControlPanel extends JPanel {
 	}
 
 	public void update(ImgPanel p) {
+
+		//toggle control panel visibility
 		if (ImgPanel.showControlPanel) this.setVisible(true);
 		else this.setVisible(false);
-
-		field.setText(String.format((String)p.selectedImg));
+		
+		field.setText(currentImg); //show current selected img to morph to
 	}
-
-	private BufferedImage getScaledImage(BufferedImage srcImg, int w, int h){
+	
+	private BufferedImage getScaledImage(BufferedImage srcImg, int w, int h)
+	{
 		BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2 = resizedImg.createGraphics();
 
@@ -164,18 +152,228 @@ public class ControlPanel extends JPanel {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+////import java.awt.BorderLayout;
+////import java.awt.Container;
+////import java.awt.FlowLayout;
+////
+////import javax.swing.JButton;
+////import javax.swing.JLabel;
+////import javax.swing.JPanel;
+////import javax.swing.JTextField;
+////import javax.swing.SwingConstants;
+////import javax.swing.border.TitledBorder;
+////
+////public class ControlPanel extends JPanel 
+////{
+////	private Container bottom, top;
+////	private JButton img1, img2, img3;
+////	private JButton morphButton;
+////	private JLabel label;
+////	private JTextField field;
+////	private Container center;
+////	
+////	public ControlPanel()
+////	{
+////		super();
+////		setBorder(new TitledBorder("Control Panel"));
+////		setComponentsAttributes();
+////		setLayout(new BorderLayout());
+////		add(bottom, BorderLayout.SOUTH);
+////		add(top, BorderLayout.NORTH);
+////
+////
+////		label = new JLabel("Selected Image");
+////		field = new JTextField(5);
+////		field.setFocusable(false);
+////		field.setHorizontalAlignment(SwingConstants.CENTER);
+////		center = new Container();
+////		center.setLayout(new FlowLayout(FlowLayout.LEFT, 12, 10));
+////		center.add(label);
+////		center.add(field);
+////
+////		add(center, BorderLayout.CENTER);
+////	}
+////}
+//
+//
+//
+//
+//
+//
+//import java.awt.BorderLayout;
+//import java.awt.Container;
+//import java.awt.FlowLayout;
+//import java.awt.Graphics;
+//import java.awt.Graphics2D;
+//import java.awt.RenderingHints;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.awt.image.BufferedImage;
+//import java.io.File;
+//
+//import javax.imageio.ImageIO;
+//import javax.swing.ImageIcon;
+//import javax.swing.JButton;
+//import javax.swing.JLabel;
+//import javax.swing.JPanel;
+//import javax.swing.JTextField;
+//import javax.swing.SwingConstants;
+//import javax.swing.border.TitledBorder;
+//
+//public class ControlPanel extends JPanel {
+//	private Container bottom, top;
+//	private JButton img1, img2, img3;
+//	private JButton morphButton;
+//	private JLabel label;
+//	private JTextField field;
+//
+//	private Container center;
+//
+//	public ControlPanel() {
+//		super();
+//		setBorder(new TitledBorder("Control Panel"));
+//		setComponentsAttributes();
+//		setLayout(new BorderLayout());
+//		add(bottom, BorderLayout.SOUTH);
+//		add(top, BorderLayout.NORTH);
+//
+//
+//		label = new JLabel("Selected Image");
+//		field = new JTextField(5);
+//		field.setFocusable(false);
+//		field.setHorizontalAlignment(SwingConstants.CENTER);
+//		center = new Container();
+//		center.setLayout(new FlowLayout(FlowLayout.LEFT, 12, 10));
+//		center.add(label);
+//		center.add(field);
+//
+//		add(center, BorderLayout.CENTER);
+//	}
+//
+//	private void setComponentsAttributes() 
+//	{	
+//		BufferedImage buff1 = null; 
+//		BufferedImage buff2 = null; 
+//		BufferedImage buff3 = null;
+//		// create img buttons
+//		try {
+//			buff1 = ImageIO.read(new File("mina_twice.jpg"));
+//			buff2 = ImageIO.read(new File("jy_twice.jpg"));
+//			buff3 = ImageIO.read(new File("sana_twice.jpg"));
+//		} catch (Exception e) {
+//			System.out.println("Can't load image");
+//		}
+//		
+//		// scale images
+//		buff1 = getScaledImage(buff1, buff1.getWidth()/10, buff1.getHeight()/10);
+//		buff2 = getScaledImage(buff2, buff2.getWidth()/10, buff2.getHeight()/10);
+//		buff3 = getScaledImage(buff3, buff3.getWidth()/10, buff3.getHeight()/10);
+//
+//		// TODO Auto-generated method stub
+//		bottom = new Container();
+//		bottom.setLayout(new FlowLayout(FlowLayout.LEFT, 12, 5));
+//
+//		ImageIcon icon1 = new ImageIcon(buff1);
+//		img1 = new JButton(icon1);
+//		img1.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				// TODO Auto-generated method stub
+////				ImgPanel.selectedImg = "Image 1";
+////				System.out.println("click");
+////				System.out.println(ImgPanel.selectedImg);
+//				ImgPanel.img1Bool = true;
+////				field.setText(String.format("Image 1"));
+////				System.out.println("Bool1: " + ImgPanel.img1Bool);
+////				System.out.println("Bool2: " + ImgPanel.img2Bool);
+////				System.out.println("Bool3: " + ImgPanel.img3Bool);
+//			}
+//		});
+//
+//		ImageIcon icon2 = new ImageIcon(buff2);
+//		img2 = new JButton(icon2);
+//		img2.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				// TODO Auto-generated method stub
+////				ImgPanel.selectedImg = "Image 2";
+////				System.out.println(ImgPanel.selectedImg);
+//				ImgPanel.img2Bool = true;
+////				field.setText(String.format("Image 2"));
+////				System.out.println("Bool1: " + ImgPanel.img1Bool);
+////				System.out.println("Bool2: " + ImgPanel.img2Bool);
+////				System.out.println("Bool3: " + ImgPanel.img3Bool);
+//			}
+//		});
+//
+//		ImageIcon icon3 = new ImageIcon(buff3);
+//		img3 = new JButton(icon3);
+//		img3.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				// TODO Auto-generated method stub
+////				ImgPanel.selectedImg = "Image 3";
+////				System.out.println(ImgPanel.selectedImg);
+//				ImgPanel.img3Bool = true;
+////				field.setText(String.format("Image 3"));
+////				System.out.println("Bool1: " + ImgPanel.img1Bool);
+////				System.out.println("Bool2: " + ImgPanel.img2Bool);
+////				System.out.println("Bool3: " + ImgPanel.img3Bool);
+//			}
+//		});
+//
+//		morphButton = new JButton("Morph");
+//		morphButton.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				// TODO Auto-generated method stub
+//				if (!ImgPanel.doMorph) ImgPanel.doMorph = true;
+//				System.out.println(ImgPanel.doMorph);
+//			}
+//		});
+//
+//		top = new Container();
+//		top.setLayout(new FlowLayout(FlowLayout.CENTER, 100, 10));
+//		top.add(img1);
+//		top.add(img2);
+//		top.add(img3);
+//		top.add(morphButton);
+//	}
+//
+//	public void update(ImgPanel p) {
+//		if (ImgPanel.showControlPanel) this.setVisible(true);
+//		else this.setVisible(false);
+//
+//		field.setText(String.format((String)p.selectedImg));
+//		System.out.println(p.selectedImg);
+//	}
+//
+//	private BufferedImage getScaledImage(BufferedImage srcImg, int w, int h){
+//		BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+//		Graphics2D g2 = resizedImg.createGraphics();
+//
+//		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+//		g2.drawImage(srcImg, 0, 0, w, h, null);
+//		g2.dispose();
+//
+//		return resizedImg;
+//	}
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
--- src/ImgPanel.java
+++ src/ImgPanel.java
@@ -4,138 +4,287 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
-
-import org.opencv.core.Mat;
-import org.opencv.imgcodecs.Imgcodecs;
+import javax.swing.Timer;
 
 public class ImgPanel extends JPanel implements ActionListener {
 
-	public String selectedImg;
+	private Timer t;
+	public boolean addFood;
+	public static boolean selectAll = false; //boolean for shift + 'D' display toggle
+	public static boolean showControlPanel = true; //hide/show control panel
+
+	public String selectedImg = "Image 1";
+	public String prevImg;
 	public static BufferedImage morphTo, img1, img2, img3;
 	public static boolean doMorph;
 	public static boolean img1Bool, img2Bool, img3Bool;
-	
+
 	private static ControlPanel cPanel;
+
+	private static String status = "Status";
 	
-	public static boolean showControlPanel = true;
-	
-	public ImgPanel(Dimension initialSize, ControlPanel cp)  
-	{
+	int w,h,x;
+
+	public ImgPanel(Dimension initialSize, ControlPanel cp) {
+		// TODO Auto-generated constructor stub
 		super();
+
 		cPanel = cp;
+		
 		selectedImg = "Image 1";
-		doMorph = false;
-		img1Bool = true;
-		img2Bool = false;
-		img3Bool = false;
+		prevImg = "Image 1";
 		
 		try {
 			img1 = ImageIO.read(new File("mina_twice.jpg"));
 			img2 = ImageIO.read(new File("jy_twice.jpg"));
 			img3 = ImageIO.read(new File("sana_twice.jpg"));
 			morphTo = ImageIO.read(new File("mina_twice.jpg"));
+			w = (int) (morphTo.getWidth()/2);
+			h = (int) (morphTo.getHeight()/2);
+			x = (this.getWidth() - morphTo.getWidth(null));
+			this.setSize(w*5+100,h*4+50);
+			System.out.println("images read");
 		} catch (Exception e) {
 			System.out.println("Can't load image");
 		}
+		
+
+		int randomSpeedx = (int) (Math.random()*14 - 7);
+		if (randomSpeedx == 0) randomSpeedx = 7;
+
+		int randomSpeedy = (int) (Math.random()*14 - 7);
+		if (randomSpeedy == 0) randomSpeedx = 7;
+
+		
+		// DO NOT TOUCH THE TIMER
+		// don't know why this timer is necessary, but it is
+		t = new Timer(33, this);
+		t.start();
 
 		setFocusable(true);
 	}
-	
-	
+
+	@Override
 	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		int w = (int) (morphTo.getWidth()/2);
-		int h = (int) (morphTo.getHeight()/2);
-		int x = (this.getWidth() - morphTo.getWidth(null));
-		
+		w = (int) (morphTo.getWidth()/2);
+		h = (int) (morphTo.getHeight()/2);
+		x = (this.getWidth() - morphTo.getWidth(null));
 		this.setSize(w*5+100,h*4+50);
-		
-		setBackground(new Color(50,50,50));
-		
 		g.drawImage(morphTo, x,0,w,h,this);
-		System.out.println("again");
-		
-		if (img1Bool) {
-			selectedImg = "Image 1";
-		}
-		
-		if (img2Bool) {
-			selectedImg = "Image 2";
-		}
-		
-		if (img3Bool) {
-			selectedImg = "Image 3";
-		}
-		
-		
-		
-		
-		if (doMorph) {
-			System.out.println("DO DA MORPHY MORPHY"); // do morphing
-			doMorph = false;
-		}
-		
 		cPanel.update(this);
 	}
-	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("please");
-		System.out.println("SYSTEM CHECK");
 		// TODO Auto-generated method stub
-		if (selectedImg == "Image 1") {
-			try {
-				morphTo = ImageIO.read(new File("mina_twice.jpg"));
-			} catch (Exception e1) {
-				System.out.println("Can't load image");
-			}
+		String imgAddress = "mina_twice.jpg";
+		
+		if (img1Bool) {
+			selectedImg = "Image 1";
+			imgAddress = "mina_twice.jpg";
+			System.out.println(selectedImg);
+			img1Bool = false;
 		}
 		
-		if (selectedImg == "Image 2") {
-			try {
-				morphTo = ImageIO.read(new File("jy_twice.jpg"));
-			} catch (Exception e1) {
-				System.out.println("Can't load image");
-			}
+		else if (img2Bool) {
+			selectedImg = "Image 2";
+			imgAddress = "jy_twice.jpg";
+			System.out.println(selectedImg);
+			img2Bool = false;
 		}
 		
-		if (selectedImg == "Image 3") {
-			try {
-				morphTo = ImageIO.read(new File("sana_twice.jpg"));
-			} catch (Exception e1) {
-				System.out.println("Can't load image");
+		else if (img3Bool) {
+			selectedImg = "Image 3";
+			imgAddress = "sana_twice.jpg";
+			System.out.println(selectedImg);
+			img3Bool = false;
+		}
+		
+		if (doMorph) { //boolean for initiating panic mode of highlighted animal
+			System.out.println("Morphing Time");
+			if (selectedImg != prevImg) {
+				System.out.println("test");
+				try {
+					morphTo = ImageIO.read(new File(imgAddress));
+				} catch (Exception e1) {
+					System.out.println("Can't load image");
+				}
 			}
+			doMorph = false;
+			prevImg = selectedImg;
 		}
 		
 		repaint();
 	}
+
+	public static void setStatus(String st) {
+		status = st;
+	}
+
+	public String getStatus() {
+		return status;
+	}
 }
 
+	
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//import java.awt.Color;
+//import java.awt.Dimension;
+//import java.awt.Graphics;
+//import java.awt.Graphics2D;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.awt.image.BufferedImage;
+//import java.io.File;
+//
+//import javax.imageio.ImageIO;
+//import javax.swing.JPanel;
+//
+//import org.opencv.core.Mat;
+//import org.opencv.imgcodecs.Imgcodecs;
+//
+//public class ImgPanel extends JPanel implements ActionListener {
+//
+//	public String selectedImg = "Image 1";
+//	public static BufferedImage morphTo, img1, img2, img3;
+//	public static boolean doMorph;
+//	public static boolean img1Bool, img2Bool, img3Bool;
+//	
+//	private static ControlPanel cPanel;
+//	
+//	public static boolean showControlPanel = true;
+//	
+//	public ImgPanel(Dimension initialSize, ControlPanel cp)  
+//	{
+//		super();
+//		cPanel = cp;
+////		selectedImg = "Image 1";
+//		doMorph = false;
+//		img1Bool = true;
+//		img2Bool = false;
+//		img3Bool = false;
+//		
+//		try {
+//			img1 = ImageIO.read(new File("mina_twice.jpg"));
+//			img2 = ImageIO.read(new File("jy_twice.jpg"));
+//			img3 = ImageIO.read(new File("sana_twice.jpg"));
+//			morphTo = ImageIO.read(new File("mina_twice.jpg"));
+//		} catch (Exception e) {
+//			System.out.println("Can't load image");
+//		}
+//
+//		setFocusable(true);
+//	}
+//	
+//	
+//	public void paintComponent(Graphics g) {
+//		int w = (int) (morphTo.getWidth()/2);
+//		int h = (int) (morphTo.getHeight()/2);
+//		int x = (this.getWidth() - morphTo.getWidth(null));
+//		
+//		this.setSize(w*5+100,h*4+50);
+//		
+//		setBackground(new Color(50,50,50));
+//		
+//		if (selectedImg == "Image 1") {
+//			try {
+//				morphTo = ImageIO.read(new File("mina_twice.jpg"));
+//			} catch (Exception e1) {
+//				System.out.println("Can't load image");
+//			}
+//		}
+//		
+//		if (selectedImg == "Image 2") {
+//			try {
+//				morphTo = ImageIO.read(new File("jy_twice.jpg"));
+//			} catch (Exception e1) {
+//				System.out.println("Can't load image");
+//			}
+//		}
+//		
+//		if (selectedImg == "Image 3") {
+//			try {
+//				morphTo = ImageIO.read(new File("sana_twice.jpg"));
+//			} catch (Exception e1) {
+//				System.out.println("Can't load image");
+//			}
+//		}
+//		
+//		g.drawImage(morphTo, x,0,w,h,this);
+//		System.out.println("again");
+//		
+//		
+//		
+//		cPanel.update(this);
+//	}
+//
+//
+//	@Override
+//	public void actionPerformed(ActionEvent e) {
+//		// TODO Auto-generated method stub
+//		String ch = e.getActionCommand();
+//		
+//		System.out.println("SYSTEM CHECK");
+//		if (img1Bool) {
+//			selectedImg = "Image 1";
+//			img2Bool = false;
+//			img3Bool = false;
+//		}
+//		
+//		if (img2Bool) {
+//			selectedImg = "Image 2";
+//			img1Bool = false;
+//			img3Bool = false;
+//		}
+//		
+//		if (img3Bool) {
+//			selectedImg = "Image 3";
+//			img2Bool = false;
+//			img1Bool = false;
+//		}
+//		
+//		if (doMorph) {
+//			System.out.println("DO DA MORPHY MORPHY"); // do morphing
+//			doMorph = false;
+//		}
+//		
+//		
+//		repaint();
+//		
+//	}
+//	
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
